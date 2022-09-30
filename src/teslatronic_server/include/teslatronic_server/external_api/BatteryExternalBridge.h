10d00472a8330369687d713175c59593435bf6c7
Zhivko Petrov <zhivko.petrov@ocado.com>
2022-09-30 14:53:06 +0300

    feat: implement battery charge action_server on the teslatronic_server

--- src/teslatronic_server/include/teslatronic_server/external_api/BatteryExternalBridge.h
+++ src/teslatronic_server/include/teslatronic_server/external_api/BatteryExternalBridge.h
@@ -1,10 +1,14 @@
 #ifndef BATTERYEXTERNALBRIDGE_H_
 #define BATTERYEXTERNALBRIDGE_H_
 
+#include <mutex>
+
 #include <rclcpp/node.hpp>
 #include <rclcpp/service.hpp>
 #include "rclcpp/timer.hpp"
+#include <rclcpp_action/rclcpp_action.hpp>
 #include <teslatronic_interfaces/srv/query_battery_info.hpp>
+#include <teslatronic_interfaces/action/charge_battery.hpp>
 
 #include "teslatronic_server/teslatronic_common/FunctionalDefines.h"
 
@@ -23,14 +27,30 @@ public:
 
 private:
   using QueryBatteryInfo = teslatronic_interfaces::srv::QueryBatteryInfo;
+  using ChargeBattery = teslatronic_interfaces::action::ChargeBattery;
+  using GoalHandleChargeBattery = rclcpp_action::ServerGoalHandle<ChargeBattery>;
 
   void handleBatterInfoQueryService(
       const std::shared_ptr<QueryBatteryInfo::Request> request,
       std::shared_ptr<QueryBatteryInfo::Response> response);
 
-  std::shared_ptr<rclcpp::Service<QueryBatteryInfo>> _batteryInfoQueryService;
+  rclcpp_action::GoalResponse handleChargeBatteryGoal(
+      const rclcpp_action::GoalUUID &uuid,
+      std::shared_ptr<const ChargeBattery::Goal> goal);
+
+  rclcpp_action::CancelResponse handleChargeBatteryCancel(
+      const std::shared_ptr<GoalHandleChargeBattery> goalHandle);
+
+  void handleChargeBatteryAccepted(
+      const std::shared_ptr<GoalHandleChargeBattery> goalHandle);
+
+  void executeGoal(const std::shared_ptr<GoalHandleChargeBattery> goalHandle);
 
   rclcpp::TimerBase::SharedPtr _batteryDepleteHeadTimer;
+  std::shared_ptr<rclcpp::Service<QueryBatteryInfo>> _batteryInfoQueryService;
+  rclcpp_action::Server<ChargeBattery>::SharedPtr _chargeBatteryActionServer;
+
+  std::mutex _batteryMutex;
   BatteryExternalBridgeOutInterface _outInterface;
 };
 
--- src/teslatronic_server/src/external_api/BatteryExternalBridge.cpp
+++ src/teslatronic_server/src/external_api/BatteryExternalBridge.cpp
@@ -54,7 +54,14 @@ int32_t BatteryExternalBridge::init(
       std::bind(&BatteryExternalBridge::handleBatterInfoQueryService, this, _1,
           _2));
 
+  _chargeBatteryActionServer = rclcpp_action::create_server<ChargeBattery>(this,
+      CHARGE_BATTERY_ACTION_NAME,
+      std::bind(&BatteryExternalBridge::handleChargeBatteryGoal, this, _1, _2),
+      std::bind(&BatteryExternalBridge::handleChargeBatteryCancel, this, _1),
+      std::bind(&BatteryExternalBridge::handleChargeBatteryAccepted, this, _1));
+
   _batteryDepleteHeadTimer = create_wall_timer(1s, [this]() {
+    std::lock_guard<std::mutex> lock(_batteryMutex);
     _outInterface.depleteHeatCb();
   });
 
@@ -64,6 +71,89 @@ int32_t BatteryExternalBridge::init(
 void BatteryExternalBridge::handleBatterInfoQueryService(
     [[maybe_unused]]const std::shared_ptr<QueryBatteryInfo::Request> request,
     std::shared_ptr<QueryBatteryInfo::Response> response) {
+  std::lock_guard<std::mutex> lock(_batteryMutex);
   const BatteryInfo &data = _outInterface.getBatteryInfoCb();
   fillBatteryInfo(data, response->battery_info);
 }
+
+rclcpp_action::GoalResponse BatteryExternalBridge::handleChargeBatteryGoal(
+    const rclcpp_action::GoalUUID &uuid,
+    std::shared_ptr<const ChargeBattery::Goal> goal) {
+  std::cout << "Received goal request with charge_turns: " << goal->charge_turns
+            << ", uuid: " << rclcpp_action::to_string(uuid) << std::endl;
+
+  _outInterface.setChargeStateCb(BatteryChargeState::ACTIVE);
+
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse BatteryExternalBridge::handleChargeBatteryCancel(
+    const std::shared_ptr<GoalHandleChargeBattery> goalHandle) {
+  std::cout << "Canceling goal with UUID: "
+            << rclcpp_action::to_string(goalHandle->get_goal_id()) << std::endl;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void BatteryExternalBridge::handleChargeBatteryAccepted(
+    const std::shared_ptr<GoalHandleChargeBattery> goalHandle) {
+  std::thread { std::bind(&BatteryExternalBridge::executeGoal, this, _1),
+      goalHandle }.detach();
+}
+
+void BatteryExternalBridge::executeGoal(
+    const std::shared_ptr<GoalHandleChargeBattery> goalHandle) {
+  using namespace std::literals;
+
+  const auto goal = goalHandle->get_goal();
+  auto feedback = std::make_shared<ChargeBattery::Feedback>();
+  auto result = std::make_shared<ChargeBattery::Result>();
+  feedback->charge_turns_left = goal->charge_turns;
+
+  std::unique_lock<std::mutex> lock(_batteryMutex);
+  const BatteryInfo &batteryData = _outInterface.getBatteryInfoCb();
+
+  while (0 < feedback->charge_turns_left) {
+    // Check if there is a cancel request
+    if (goalHandle->is_canceling()) {
+      result->success = true;
+
+      goalHandle->canceled(result);
+      std::cout << "Goal with UUID: "
+                << rclcpp_action::to_string(goalHandle->get_goal_id())
+                << " was cancelled" << std::endl;
+
+      fillBatteryInfo(batteryData, result->battery_info);
+      _outInterface.setChargeStateCb(BatteryChargeState::IDLE);
+      return;
+    }
+
+    const bool success = _outInterface.chargeBatterySingleTurnCb();
+    if (!success) {
+      result->success = false;
+      result->error_reason = "Goal with UUID: ";
+      result->error_reason.append(
+          rclcpp_action::to_string(goalHandle->get_goal_id())).append(
+          " was cancelled due to maximum heat reached");
+      std::cout << result->error_reason << std::endl;
+
+      fillBatteryInfo(batteryData, result->battery_info);
+      goalHandle->succeed(result);
+      _outInterface.setChargeStateCb(BatteryChargeState::IDLE);
+      return;
+    }
+
+    --feedback->charge_turns_left;
+    fillBatteryInfo(batteryData, feedback->battery_info);
+    goalHandle->publish_feedback(feedback);
+
+    lock.unlock();
+    std::this_thread::sleep_for(500ms);
+    lock.lock();
+  }
+
+  fillBatteryInfo(batteryData, result->battery_info);
+  result->success = true;
+  goalHandle->succeed(result);
+  _outInterface.setChargeStateCb(BatteryChargeState::IDLE);
+}
+
